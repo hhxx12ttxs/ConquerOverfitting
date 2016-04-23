2a9b694c44c68aeb422450b80cc76d87c3c492a9
yjxxtd <yjxxtd@yeah.net>
2016-04-23 10:44:21 -0600

    merget interval

--- src/main/java/cn/edu/pku/sei/plde/conqueroverfitting/boundary/model/Interval.java
+++ src/main/java/cn/edu/pku/sei/plde/conqueroverfitting/boundary/model/Interval.java
@@ -4,9 +4,21 @@ package cn.edu.pku.sei.plde.conqueroverfitting.boundary.model;
  * Created by yjxxtd on 4/23/16.
  */
 public class Interval {
-    public double left;
-    public double right;
-    public boolean leftColose;//true [,false (
+    public double leftBoundary;
+    public double rightBoundary;
+    public boolean leftClose;//true [,false (
     public boolean rightClose;//true ], false )
+    public Interval(double leftBoundary, double rightBoundary, boolean leftClose, boolean rightClose){
+        this.leftBoundary = leftBoundary;
+        this.rightBoundary = rightBoundary;
+        this.leftClose = leftClose;
+        this.rightClose = rightClose;
+    }
 
+    public Interval(Interval interval){
+        this.leftBoundary = interval.leftBoundary;
+        this.rightBoundary = interval.rightBoundary;
+        this.leftClose = interval.leftClose;
+        this.rightClose = interval.rightClose;
+    }
 }
