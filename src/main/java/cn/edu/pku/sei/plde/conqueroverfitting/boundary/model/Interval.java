188aae7f7efc5902cc012a745259added4621a84
yjxxtd <yjxxtd@yeah.net>
2016-04-23 10:17:45 -0600

    interval

---
+++ src/main/java/cn/edu/pku/sei/plde/conqueroverfitting/boundary/model/Interval.java
@@ -0,0 +1,12 @@
+package cn.edu.pku.sei.plde.conqueroverfitting.boundary.model;
+
+/**
+ * Created by yjxxtd on 4/23/16.
+ */
+public class Interval {
+    public double left;
+    public double right;
+    public boolean leftColose;//true [,false (
+    public boolean rightClose;//true ], false )
+
+}
