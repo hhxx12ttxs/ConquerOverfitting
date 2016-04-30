f64f8c791057102d59db21f8c587d7c9ac050169
yanrunfa <yanrunfa@outlook.com>
2016-04-30 11:57:03 +0800

    final test

--- src/main/java/cn/edu/pku/sei/plde/conqueroverfitting/localization/gzoltar/StatementExt.java
+++ src/main/java/cn/edu/pku/sei/plde/conqueroverfitting/localization/gzoltar/StatementExt.java
@@ -87,9 +87,9 @@ public class StatementExt extends Statement  {
 
     @Override
     public double getSuspiciousness() {
-        //if (this.getLabel().contains("getOffsetFromLocal")){
-        //    return getSuspiciousness(this.defaultMetric)*10;
-        //}
+        if (this.getLabel().contains("getOffsetFromLocal")){
+            return getSuspiciousness(this.defaultMetric)*10;
+        }
         return getSuspiciousness(this.defaultMetric);
     }
 
